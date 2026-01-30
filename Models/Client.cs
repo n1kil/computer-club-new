92949af31fe0e7c3f9c20f361e7aa7189199786f
n1kil <nikchernyavsky@bk.ru>
2026-01-30 10:57:26 +1000

    add client class

---
+++ Models/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computer_club.Models
+{
+    class Client
+    {
+        private int _money;
+        private int _moneyToPay;
+
+        public string Name { get; }
+        public int DesiredMinutes { get; }
+
+        public Client(int money, string name, Random random)
+        {
+            _money = money;
+            Name = name;
+            DesiredMinutes = random.Next(10, 30);
+        }
+
+        public bool CanPay(Computer computer)
+        {
+            _moneyToPay = DesiredMinutes * computer.PricePerMinute;
+
+            if (_money >= _moneyToPay)
+                return true;
+
+            _moneyToPay = 0;
+            return false;
+        }
+
+        public int Pay()
+        {
+            _money -= _moneyToPay;
+            return _moneyToPay;
+        }
+
+        public enum Names
+        {
+            Oliver, Emma, Liam, Ava, Noah,
+            Sophia, Ethan, Isabella, Lucas, Mia,
+            Mason, Amelia, Logan, Charlotte, Jacob,
+            Harper, Elijah, Evelyn, Alexander, Abigail
+        }
+    }
+}
